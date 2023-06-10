f209e400c17c48ff2939267814e12d6f54db2f0c
masandrei <134847765+masandrei@users.noreply.github.com>
2023-06-10 16:53:14 +0200

    weisfeilerLehman.cpp
    Ya v tilte

--- src/weisfeilerLehman.cpp
+++ src/weisfeilerLehman.cpp
@@ -1,30 +1,26 @@
 #include "ValidateIsomorphism.h"
-bool weisfeilerLehman(std::vector<std::vector<int>> adjMat1, std::vector<std::vector<int>> adjMat2) {
-    if(adjMat1.size() != adjMat2.size())return false;
-    std::vector<int>v1 =  getCanonical(adjMat1);
-    std::vector<int>v2 =  getCanonical(adjMat2);
-    sort(v1);
-    sort(v2);
-    return v1 == v2;
-}
+#include <vector>
+#include <cmath>
 std::vector<int> getDegree(std::vector<std::vector<int>> adjMat){
     std::vector<int> degrees;
     for(int i = 0; i < adjMat.size();i++){
         int deg = 0;
         for(int j = 0; i < adjMat.size();j++){
-            if(adjMat1[i][j] != 0)deg++;
+            if(adjMat[i][j] != 0)deg++;
         }
         degrees.push_back(deg);
     }
     return degrees;
 }
-std::vector<int> getCanonical(std::vector<vector<int>> adjMat){
+std::vector<int> getCanonical(std::vector<std::vector<int>> adjMat){
+    std::vector<int> degrees;
     std::vector<int> values = getDegree(adjMat);
-    for(int i = 0; i < log(adjMat1.size());i++){
-        for(int i = 0; i < adjMat1.size();i++){
+    degrees = values;
+    for(int i = 0; i < log(adjMat.size());i++){
+        for(int i = 0; i < adjMat.size();i++){
             int deg = 0;
-            for(int j = 0; i < adjMat1.size();j++){
-                if(adjMat1[i][j] != 0){
+            for(int j = 0; i < adjMat.size();j++){
+                if(adjMat[i][j] != 0){
                     deg += degrees[j];
                 } 
             }
@@ -33,6 +29,15 @@ std::vector<int> getCanonical(std::vector<vector<int>> adjMat){
     }
     return values;
 }
+bool weisfeilerLehman(std::vector<std::vector<int>> adjMat1, std::vector<std::vector<int>> adjMat2) {
+    if(adjMat1.size() != adjMat2.size())return false;
+    std::vector<int>v1 =  getCanonical(adjMat1);
+    std::vector<int>v2 =  getCanonical(adjMat2);
+    sort(v1.begin(),v1.end());
+    sort(v2.begin(),v2.end());
+    return v1 == v2;
+}
+
 bool ValidateIsomorphism::weisfeilerLehmanMethod() {
     return weisfeilerLehman(adjMat1, adjMat2);
 }
