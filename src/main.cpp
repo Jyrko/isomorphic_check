c18b1ff5b2c5cc0534f9af2c11fa6f211856f062
masandrei <134847765+masandrei@users.noreply.github.com>
2023-06-10 17:20:31 +0200

    main.cpp
    Additional test case

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,26 @@ int main() {
             {1, 0, 0, 1, 0}
         }
         );
+    //non-isomorhic grapp ,which will be considered as isomorphic by Weisfeiller-Lehman algorithm
+    ValidateIsomorphism validator2 = ValidateIsomorphism(
+        {
+            {0, 1, 0, 0, 0, 1},
+            {1, 0, 1, 0, 0, 0},
+            {0, 1, 0, 1, 0, 0},
+            {0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0},
+            
+        }, 
+        {   
+            {0, 1, 1, 0, 0, 0},
+            {1, 0, 1, 0, 0, 0},
+            {1, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 1},
+            {0, 0, 0, 1, 0, 1},
+            {0, 0, 0, 1, 1, 0},
+        }
+        );
 
     std::cout << validator.bruteForceMethod() << std::endl;
     std::cout << validator.weisfeilerLehmanMethod() << std::endl;
